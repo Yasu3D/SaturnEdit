48a31d38ab3a2b0c158d63ddd168d09f9ceff321
Yasu3D <64372170+Yasu3D@users.noreply.github.com>
2025-05-30 22:36:27 +0200

    add window chromes, more style

--- Styles/Controls/WindowChrome.axaml.cs
+++ Styles/Controls/WindowChrome.axaml.cs
@@ -2,11 +2,11 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
-namespace SaturnEdit.Views;
+namespace SaturnEdit.Styles.Controls;
 
-public partial class CustomWindowButtons : UserControl
+public partial class WindowChrome : UserControl
 {
-    public CustomWindowButtons()
+    public WindowChrome()
     {
         InitializeComponent();
     }
