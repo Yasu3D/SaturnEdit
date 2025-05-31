6e58cedee5e7b7926a18e314d6684c8044cbdec8
Yasu3D <64372170+Yasu3D@users.noreply.github.com>
2025-06-01 00:17:34 +0200

    implement some styles and start with localization

--- App.axaml.cs
+++ App.axaml.cs
@@ -1,6 +1,8 @@
+using System;
+using System.Linq;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Markup.Xaml;
+using Avalonia.Markup.Xaml.Styling;
 
 namespace SaturnEdit;
 
@@ -14,5 +16,18 @@ public partial class App : Application
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    public static void SetLocale(string key)
+    {
+        if (Current == null) return;
+        
+        ResourceInclude? oldLocale = Current.Resources.MergedDictionaries.OfType<ResourceInclude>().FirstOrDefault(x => x.Source?.OriginalString?.Contains("Locales") ?? false);
+        
+        Uri uri = new($"avares://SaturnEdit/Assets/Locales/{key}.axaml");
+        ResourceInclude newLocale = new(uri) { Source = uri };
+
+        if (oldLocale != null) Current.Resources.MergedDictionaries.Remove(oldLocale);
+        Current.Resources.MergedDictionaries.Add(newLocale);
     }
 }
