30357f0d309176137b26309543ca0efe86047f30
Yasu3D <64372170+Yasu3D@users.noreply.github.com>
2025-06-21 14:59:43 +0200

    basic cursor menu design

---
+++ SaturnEdit/Views/Main/ChartEditor/Tabs/CursorView.axaml.cs
@@ -0,0 +1,26 @@
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace SaturnEdit.Views.Main.ChartEditor.Tabs;
+
+public partial class CursorView : UserControl
+{
+    public CursorView()
+    {
+        InitializeComponent();
+    }
+
+    private void SliderPosition_OnValueChanged(object? sender, RangeBaseValueChangedEventArgs e)
+    {
+        if (sender == null) return;
+
+        TextBlockPosition.Text = $"{(int)SliderPosition.Value}";
+    }
+    
+    private void SliderSize_OnValueChanged(object? sender, RangeBaseValueChangedEventArgs e)
+    {
+        if (sender == null) return;
+        
+        TextBlockSize.Text = $"{(int)SliderSize.Value}";
+    }
+}
